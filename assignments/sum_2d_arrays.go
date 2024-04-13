b5d53fe9bd34c4ec71567406be77aa52b6232644
mohamedfawas <fawastmh@gmail.com>
2024-04-13 06:54:09 +0530

    update : new problems

--- assignments/sum_2d_arrays.go
+++ assignments/sum_2d_arrays.go
@@ -41,17 +41,17 @@ func main() {
 	fmt.Println(" the created first array is : ", array1)
 	fmt.Println(" the created second array is : ", array2)
 
-	//swap
-	for i := 0; i < row; i++ {
-		for j := 0; j < col; j++ {
-			var temp int = array1[i][j]
-			array1[i][j] = array2[i][j]
-			array2[i][j] = temp
-		}
+	// create second array
+	sum_array := make([][]int, row)
+	for i := range sum_array {
+		sum_array[i] = make([]int, col)
 	}
 
-	fmt.Println("==============after swap=================")
-	fmt.Println(" the created first array is : ", array1)
-	fmt.Println(" the created second array is : ", array2)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			sum_array[i][j] = array1[i][j] + array2[i][j]
+		}
+	}
+	fmt.Println(" the array created after adding values in given two arrays : ", sum_array)
 
 }
