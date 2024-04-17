17a3f076db7e7e91fc957827b22b488244025c01
mohamedfawas <fawastmh@gmail.com>
2024-04-17 12:09:08 +0530

    maps workout

--- gobyexample/maps.go
+++ gobyexample/maps.go
@@ -13,8 +13,15 @@ func main() {
 
 	v1 := m["k1"]
 	fmt.Println("value v1 : ", v1)
-	v2 := m["k1"]
-	fmt.Println("value v2 : ", v2)
+	v3 := m["k3"]
+	fmt.Println("value v2 : ", v3)
 
 	fmt.Println("length of m is ", len(m))
+
+	delete(m, "k2")
+	fmt.Println("the map after deleting the value k2 : ", m)
+
+	_, prs := m["k1"] // returns true or false, which means value is present or not
+	fmt.Println("prs:", prs)
+	//fmt.Println(m["k1"])
 }
